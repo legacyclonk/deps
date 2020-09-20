58fc7e02d71ef1e7b3b0607c87c24152c30b4819
Markus Mittendrein <git@maxmitti.tk>
2020-09-21 00:24:22 +0200

    Build SDL2

---
+++ scripts/SDL2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+VERSION=2.0.12
+
+curl -L https://www.libsdl.org/release/SDL2-"$VERSION".tar.gz | tar xz
+
+mkdir build
+pushd build
+
+cmake ../SDL2-"$VERSION" -DBUILD_SHARED_LIBS=Off $CMAKE_CONFIGURE_ARGS
+cmake --build . $CMAKE_BUILD_ARGS
+
+LIBS=("$PWD"/"${OUTDIR}"*"$LIBSUFFIX")
+popd
+pushd SDL2-"$VERSION"
+INCLUDES=("$PWD"/include/*)
+popd
