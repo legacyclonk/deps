7550f59bdb5e3aee8af4627b9c3abdb0e9e529d3
Markus Mittendrein <git@maxmitti.tk>
2020-09-26 01:17:29 +0200

    Build libjpeg from libjpeg-turbo

---
+++ scripts/libjpeg-turbo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+VERSION=2.0.5
+curl -L https://downloads.sourceforge.net/project/libjpeg-turbo/"$VERSION"/libjpeg-turbo-"$VERSION".tar.gz | tar xz
+
+mkdir build
+pushd build
+
+cmake ../libjpeg-turbo-"$VERSION" -DENABLE_SHARED=Off -DWITH_JPEG8=On -DWITH_TURBOJPEG=Off -DBUILD_SHARED_LIBS=Off $CMAKE_CONFIGURE_ARGS
+cmake --build . $CMAKE_BUILD_ARGS
+cmake --install . $CMAKE_BUILD_ARGS
+
+
+popd
