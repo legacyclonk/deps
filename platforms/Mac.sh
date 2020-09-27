14ba5064db80f61af52ab543b85b27f9dcd0589e
Markus Mittendrein <git@maxmitti.tk>
2020-09-27 18:24:37 +0200

    Mac: Target older Mac version

--- platforms/Mac.sh
+++ platforms/Mac.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "::set-env name=CMAKE_CONFIGURE_ARGS::-DCMAKE_BUILD_TYPE=Release"
+echo "::set-env name=CMAKE_CONFIGURE_ARGS::-DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9"
 echo "::set-env name=MAKE_CMD::make -j$(nproc)"
+echo "::set-env name=LINK_FLAGS::-mmacosx-version-min=10.9"
+echo "::set-env name=C_XX_FLAGS::-mmacosx-version-min=10.9"
--- scripts/openssl.sh
+++ scripts/openssl.sh
@@ -9,7 +9,7 @@ curl -L https://www.openssl.org/source/openssl-"$VERSION".tar.gz | tar -xz
 mkdir build
 pushd build
 
-../openssl-"$VERSION"/config --prefix="$OUTPUT_DIR" no-shared no-tests
+CFLAGS="$C_XX_FLAGS" CXXFLAGS="$C_XX_FLAGS" LDFLAGS="$LINK_FLAGS" ../openssl-"$VERSION"/config --prefix="$OUTPUT_DIR" no-shared no-tests
 $MAKE_CMD
 $MAKE_CMD install_sw
 
