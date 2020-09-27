dfcbac979b5c0295f2571336dbd44103c21b20a8
Markus Mittendrein <git@maxmitti.tk>
2020-09-27 17:55:09 +0200

    Freetype: Disable brotli and bz2

--- scripts/freetype.sh
+++ scripts/freetype.sh
@@ -9,7 +9,7 @@ curl -L https://download-mirror.savannah.gnu.org/releases/freetype/freetype-"$VE
 mkdir build
 pushd build
 
-cmake ../freetype-"$VERSION" -DBUILD_SHARED_LIBS=Off "$@" $CMAKE_CONFIGURE_ARGS
+cmake ../freetype-"$VERSION" -DCMAKE_DISABLE_FIND_PACKAGE_BrotliDec=TRUE -DCMAKE_DISABLE_FIND_PACKAGE_BZip2=TRUE -DBUILD_SHARED_LIBS=Off "$@" $CMAKE_CONFIGURE_ARGS
 cmake --build . $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
