bcfb43c2085726186278e2341a260b1e050ccd35
Markus Mittendrein <git@maxmitti.tk>
2020-10-08 21:07:45 +0200

    Harfbuzz: Generate pkgconfig file

--- scripts/harfbuzz.sh
+++ scripts/harfbuzz.sh
@@ -13,5 +13,9 @@ cmake ../harfbuzz-"$VERSION" -DHB_BUILD_SUBSET=Off -DHB_HAVE_FREETYPE=On -DBUILD
 cmake --build . $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
+# Because the CMakeLists.txt doesn't take care of it...
+if [ "$OS" = "Linux" ]; then
+	sed -e "s@%prefix%@$OUTPUT_DIR@g" -e "s@%exec_prefix%@$OUTPUT_DIR@g" -e 's@%libdir%@${prefix}/lib@g' -e 's@%includedir%@${prefix}/include@g' -e "s/%VERSION%/$VERSION/g" -e 's/%libs_private%/-pthread/g' -e 's/%requires_private%/freetype2/g' ../harfbuzz-"$VERSION"/src/harfbuzz.pc.in > "$OUTPUT_DIR/lib/pkgconfig/harfbuzz.pc"
+fi
 
 popd
