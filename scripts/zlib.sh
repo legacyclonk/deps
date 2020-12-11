4e0d4e84a59af8d759b5d4d111ca39a41abaec27
Markus Mittendrein <git@maxmitti.tk>
2020-12-11 22:47:28 +0100

    Improve zlib script

--- scripts/zlib.sh
+++ scripts/zlib.sh
@@ -4,7 +4,7 @@ set -e
 
 VERSION=1.2.11
 
-curl -L http://zlib.net/zlib-"$VERSION".tar.gz | tar -xz
+curl -L https://zlib.net/zlib-"$VERSION".tar.gz | tar -xz
 
 mkdir build
 pushd build
@@ -13,6 +13,6 @@ cmake ../zlib-"$VERSION" -DBUILD_SHARED_LIBS=Off $CMAKE_CONFIGURE_ARGS
 cmake --build . $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
-rm -f $OUTPUT_DIR/lib/{libz.so*,libz*.dylib,zlib.lib}
+rm -f "$OUTPUT_DIR"/lib/{libz.so*,libz*.dylib,zlib.lib}
 
 popd
