b24d17328ab577093a595465dff84db0487a2b74
Markus Mittendrein <maxmitti@maxmitti.tk>
2021-12-10 22:42:33 +0100

    Remove outdated comment

--- scripts/fluidsynth.sh
+++ scripts/fluidsynth.sh
@@ -9,7 +9,6 @@ curl -L https://github.com/FluidSynth/fluidsynth/archive/v"$VERSION".tar.gz | ta
 mkdir build
 pushd build
 
-# the version after 2.2.3 will have an option to disable OpenMP more cleanly
 cmake ../fluidsynth-"$VERSION" -DLIB_SUFFIX="" -DBUILD_SHARED_LIBS=Off -Denable-aufile=Off -Denable-libinstpatch=Off -Denable-libsndfile=Off -Denable-midishare=Off -Denable-sdl2=On -Denable-systemd=Off -Denable-readline=Off -Denable-alsa=Off -Denable-pulseaudio=Off -Denable-oss=Off -Denable-openmp=Off -DDCMAKE_SKIP_RPATH=On $CMAKE_CONFIGURE_ARGS
 cmake --build . --target libfluidsynth $CMAKE_BUILD_ARGS
 touch src/fluidsynth
