acbf6d98bfd17b1ab36dd9521e8e0f8e68c5e8d7
Markus Mittendrein <maxmitti@maxmitti.tk>
2022-05-08 17:20:28 +0200

    Add libffi, a glib dependency

---
+++ scripts/libffi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+VERSION=3.4.2
+
+curl -L https://github.com/libffi/libffi/releases/download/v"$VERSION"/libffi-"$VERSION".tar.gz | tar -xz
+
+pushd libffi-"$VERSION"
+
+CFLAGS="$C_XX_FLAGS" CXXFLAGS="$C_XX_FLAGS" LDFLAGS="$LINK_FLAGS" ./configure --prefix="$OUTPUT_DIR" --disable-docs --disable-shared --enable-static=yes --disable-multi-os-directory
+$MAKE_CMD
+$MAKE_CMD install
+
+license libffi "LICENSE"
+
+popd
