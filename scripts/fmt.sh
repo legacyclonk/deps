f8aeb76f5550c7f2a81591154ab8ac7e2ca38176
Jan Heberer <jangc@t-online.de>
2022-05-19 20:12:05 +0200

    Add fmtlib

---
+++ scripts/fmt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+VERSION=8.1.1
+
+curl -L -o fmt.zip "https://github.com/fmtlib/fmt/releases/download/$VERSION/fmt-$VERSION.zip"
+7z x fmt.zip
+
+mkdir build
+pushd build
+
+cmake "../fmt-$VERSION" -DBUILD_SHARED_LIBS=OFF -DFMT_DOC=OFF -DFMT_TEST=OFF $CMAKE_CONFIGURE_ARGS
+cmake --build . $CMAKE_BUILD_ARGS
+cmake --install . $CMAKE_BUILD_ARGS
+
+popd
+
+license fmt "fmt-$VERSION/LICENSE.rst" MIT
