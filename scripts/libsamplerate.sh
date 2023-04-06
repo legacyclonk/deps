fff77f4eb3bfad8db7bf6a235db2f5150953e06b
Jan Heberer <jangc@t-online.de>
2023-04-06 19:50:11 +0200

    Add libsamplerate

---
+++ scripts/libsamplerate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+VERSION=0.2.2
+
+curl -L "https://github.com/libsndfile/libsamplerate/releases/download/$VERSION/libsamplerate-$VERSION.tar.xz" | tar -xJ
+
+mkdir build
+pushd build
+
+cmake "../libsamplerate-$VERSION" \
+-DBUILD_TESTING=Off \
+-DLIBSAMPLERATE_EXAMPLES=Off \
+$CMAKE_CONFIGURE_ARGS
+
+cmake --build . $CMAKE_BUILD_ARGS
+cmake --install . $CMAKE_BUILD_ARGS
+
+popd
+
+license libsamplerate "libsamplerate-$VERSION/COPYING" 'BSD 2-Clause'
