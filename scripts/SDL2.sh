e5ad9c00e7fc348b6bd4cbaf1adfb2ee675d1048
Jan Heberer <jangc@t-online.de>
2023-04-06 19:50:14 +0200

    SDL: pass one CMake option per line

--- scripts/SDL2.sh
+++ scripts/SDL2.sh
@@ -14,7 +14,12 @@ pushd build
 
 perl -i -pe's/SDL2-static/SDL2/g' ../SDL2-"$VERSION"/CMakeLists.txt
 
-cmake ../SDL2-"$VERSION" -DSDL_RENDER_METAL=Off -DSDL_METAL=Off -DSDL_TEST=Off $CMAKE_CONFIGURE_ARGS
+cmake ../SDL2-"$VERSION" \
+-DSDL_METAL=Off \
+-DSDL_RENDER_METAL=Off \
+-DSDL_TEST=Off \
+$CMAKE_CONFIGURE_ARGS
+
 cmake --build . $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
