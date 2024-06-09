ab54fb19144f4ae47ed0487a932e15a63ee9eee9
George Tokmaji <tokmajigeorge@gmail.com>
2024-06-09 13:33:23 +0200

    Test

--- platforms/Mac.sh
+++ platforms/Mac.sh
@@ -5,6 +5,7 @@ brew update
 brew install llvm@18 ninja
 
 echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/platforms/clang16_mac.cmake" >> $GITHUB_ENV
+echo "MACOSX_DEPLOYMENT_TARGET=10.9" >> $GITHUB_ENV
 echo "MAKE_CMD=make -j$(sysctl -n hw.logicalcpu)" >> $GITHUB_ENV
 echo "LINK_FLAGS=-mmacosx-version-min=10.9" >> $GITHUB_ENV
 echo "C_XX_FLAGS=-mmacosx-version-min=10.9" >> $GITHUB_ENV
--- scripts/libpng.sh
+++ scripts/libpng.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-VERSION=1.6.40
+VERSION=1.6.43
 
 until curl -L https://downloads.sourceforge.net/sourceforge/libpng/libpng-"$VERSION".tar.gz | tar -xz; do sleep 1; done
 
 mkdir build
 pushd build
 
-cmake ../libpng-"$VERSION" -DPNG_SHARED=Off -DPNG_TESTS=Off -DSKIP_INSTALL_PROGRAMS=On $CMAKE_CONFIGURE_ARGS
+cmake ../libpng-"$VERSION" -DPNG_SHARED=Off -DPNG_TESTS=Off -DSKIP_INSTALL_PROGRAMS=On --trace-expand --trace-source=CMakeLists.txt $CMAKE_CONFIGURE_ARGS
 cmake --build . --target png_static $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
