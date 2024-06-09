843cc3f7fae740a64d5e7d420fd876212293f98f
George Tokmaji <tokmajigeorge@gmail.com>
2024-06-09 09:23:17 +0200

    Use LLVM 18 and gcc 14

--- platforms/Mac.sh
+++ platforms/Mac.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" >> $GITHUB_ENV
+export HOMEBREW_NO_INSTALL_CLEANUP=1
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+brew update
+brew install llvm@18 ninja
+
+echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/platforms/clang16_mac.cmake" >> $GITHUB_ENV
 echo "MAKE_CMD=make -j$(sysctl -n hw.logicalcpu)" >> $GITHUB_ENV
 echo "LINK_FLAGS=-mmacosx-version-min=10.9" >> $GITHUB_ENV
 echo "C_XX_FLAGS=-mmacosx-version-min=10.9" >> $GITHUB_ENV
