8a53519489de3eb5ec76cf45e25ffc4e8cb07169
George Tokmaji <tokmajigeorge@gmail.com>
2024-06-09 09:23:17 +0200

    Use LLVM 18 and gcc 14

---
+++ platforms/clang16_mac.cmake
@@ -0,0 +1,10 @@
+cmake_minimum_required(VERSION 3.20)
+set(CMAKE_C_COMPILER "/usr/local/opt/llvm/bin/clang")
+set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm/bin/clang++")
+
+set(CMAKE_C_FLAGS_INIT "-fexperimental-library -Wno-parentheses")
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++ ${CMAKE_C_FLAGS_INIT}")
+set(CMAKE_OBJCXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-L/usr/local/opt/llvm/lib/c++")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-L/usr/local/opt/llvm/lib/c++")
