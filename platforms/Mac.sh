e395ec89889ff46352c33ae62c2f1e23a633543d
George Tokmaji <tokmajigeorge@gmail.com>
2024-11-16 16:42:08 +0100

    macOS: Use macos-13 and LLVM 19

--- platforms/Mac.sh
+++ platforms/Mac.sh
@@ -2,7 +2,7 @@
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 brew update
-brew install llvm@18 ninja
+brew install llvm@19 ninja
 
 echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/platforms/clang16_mac.cmake" >> $GITHUB_ENV
 echo "MAKE_CMD=make -j$(sysctl -n hw.logicalcpu)" >> $GITHUB_ENV
