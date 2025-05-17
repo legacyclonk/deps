772462171a2776931fa6162b3c1e75519d498135
Markus Mittendrein <git@maxmitti.at>
2025-05-17 23:54:46 +0200

    Fix SDL2_mixer patches

--- scripts/SDL2_mixer.sh
+++ scripts/SDL2_mixer.sh
@@ -9,8 +9,7 @@ curl -L https://github.com/libsdl-org/SDL_mixer/releases/download/release-"$VERS
 pushd SDL2_mixer-"$VERSION"
 
 patch -p1 <"$PATCH_DIR/SDL2_mixer.patch"
-patch -p1 <"$PATCH_DIR/SDL2_mixer_memory_waste.patch"
-rm "cmake/FindMPG123.cmake"
+rm "cmake/Findmpg123.cmake"
 
 mkdir build
 pushd build
