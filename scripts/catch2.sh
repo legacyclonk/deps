56f0399ed7713cc201c938e98a1bc62cd485a1d7
Markus Mittendrein <git@maxmitti.at>
2025-05-17 16:09:11 +0200

    Update Catch2

--- scripts/catch2.sh
+++ scripts/catch2.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-VERSION=v3.4.0
+VERSION=3.8.1
 
-git clone --depth 1 --branch $VERSION https://github.com/catchorg/Catch2 Catch2
+curl -L "https://github.com/catchorg/Catch2/archive/refs/tags/v$VERSION.tar.gz" | tar xz
 
 mkdir build
 pushd build
 
-cmake ../Catch2 -DBUILD_TESTING=Off $CMAKE_CONFIGURE_ARGS
+cmake "../Catch2-$VERSION" -DBUILD_TESTING=Off $CMAKE_CONFIGURE_ARGS
 cmake --build . $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
 popd
 
-license Catch2 Catch2/LICENSE.txt
+license Catch2 "Catch2-$VERSION/LICENSE.txt"
