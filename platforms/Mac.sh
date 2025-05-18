5c93906d77e6e8cc3e0a1daf14bee4fab39c6c80
Markus Mittendrein <git@maxmitti.at>
2025-05-18 12:45:58 +0200

    Remove incorrect clang version from toolchain name

--- platforms/Mac.sh
+++ platforms/Mac.sh
@@ -4,7 +4,7 @@ export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 brew update
 brew install llvm@20 ninja
 
-echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/platforms/clang16_mac.cmake" >> $GITHUB_ENV
+echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/platforms/clang_mac.cmake" >> $GITHUB_ENV
 echo "MAKE_CMD=make -j$(sysctl -n hw.logicalcpu)" >> $GITHUB_ENV
 echo "LINK_FLAGS=-mmacosx-version-min=10.9" >> $GITHUB_ENV
 echo "C_XX_FLAGS=-mmacosx-version-min=10.9" >> $GITHUB_ENV
