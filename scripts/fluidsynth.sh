ac93618d321b38ed27a8ebe59e893e927d2eeaca
Markus Mittendrein <git@maxmitti.at>
2025-05-18 12:41:27 +0200

    Fluidsynth: Patch to remove installation of binary

--- scripts/fluidsynth.sh
+++ scripts/fluidsynth.sh
@@ -6,15 +6,17 @@ VERSION=2.4.6
 
 curl -L https://github.com/FluidSynth/fluidsynth/archive/v"$VERSION".tar.gz | tar -xz
 
+pushd fluidsynth-"$VERSION"
+patch -p1 <"$PATCH_DIR/Fluidsynth_no_binary.patch"
+popd
+
 mkdir build
 pushd build
 
+
 cmake ../fluidsynth-"$VERSION" -DLIB_SUFFIX="" -Denable-aufile=Off -Denable-libinstpatch=Off -Denable-libsndfile=Off -Denable-midishare=Off -Denable-sdl2=On -Denable-systemd=Off -Denable-readline=Off -Denable-alsa=Off -Denable-pulseaudio=Off -Denable-oss=Off -Denable-openmp=Off -Denable-ipv6=Off -Denable-network=Off -DCMAKE_SKIP_RPATH=On $CMAKE_CONFIGURE_ARGS
 cmake --build . --target libfluidsynth $CMAKE_BUILD_ARGS
-touch src/fluidsynth
 cmake --install . $CMAKE_BUILD_ARGS
-
-rm $OUTPUT_DIR/bin/fluidsynth
 
 popd
 
