8af5d7bba401bca12f3a3753bdb29f4957402717
Markus Mittendrein <git@maxmitti.at>
2025-09-13 21:18:36 +0200

    libvorbis: Set CMAKE_POLICY_VERSION_MINIMUM=3.5 for now

--- scripts/libvorbis.sh
+++ scripts/libvorbis.sh
@@ -9,7 +9,7 @@ curl -L https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-"$VERSION".tar
 mkdir build
 pushd build
 
-cmake ../libvorbis-"$VERSION" $CMAKE_CONFIGURE_ARGS
+cmake ../libvorbis-"$VERSION" -DCMAKE_POLICY_VERSION_MINIMUM=3.5 $CMAKE_CONFIGURE_ARGS
 cmake --build . $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
