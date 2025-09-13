3494b40d34e6ee23de4ed423e39040a8adce8060
Markus Mittendrein <git@maxmitti.at>
2025-09-13 17:54:04 +0200

    Update boost

--- scripts/boost.sh
+++ scripts/boost.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=1.88.0
+VERSION=1.89.0
 curl -L "https://github.com/boostorg/boost/releases/download/boost-$VERSION/boost-$VERSION-b2-nodocs.tar.gz" | tar xz
 
 pushd boost-$VERSION
