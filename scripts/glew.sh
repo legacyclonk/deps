8acc5ab4025da880dadb70b5578b16075a0ad27b
Markus Mittendrein <git@maxmitti.at>
2025-09-13 18:16:32 +0200

    Patch glew CMakeLists.txt to reference -framework OpenGL on Mac instead of hard-coding the SDK path

--- scripts/glew.sh
+++ scripts/glew.sh
@@ -6,12 +6,17 @@ VERSION=2.2.0
 
 until curl -L https://downloads.sourceforge.net/glew/glew-"$VERSION".tgz | tar -xz; do sleep 1; done
 
-pushd glew-"$VERSION"/build
+pushd glew-"$VERSION"
+
+patch -p1 <"$PATCH_DIR/glew_opengl_framework.patch"
+
+pushd build
 
 cmake ./cmake -DBUILD_UTILS=Off $CMAKE_CONFIGURE_ARGS
 cmake --build . --target glew_s $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
+popd
 popd
 
 license GLEW "glew-$VERSION/LICENSE.txt"
