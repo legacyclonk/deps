3d3eadb4503b3dab2dbd33886f5be7f6ada25dee
Markus Mittendrein <git@maxmitti.at>
2026-05-02 19:30:02 +0200

    Mac: Query brew for LLVM prefix

--- platforms/Mac.sh
+++ platforms/Mac.sh
@@ -2,7 +2,11 @@
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 brew update
-brew install llvm@22 ninja
+brew install "llvm@$LLVM_VERSION" ninja
+LLVM_PREFIX="$(brew --prefix llvm@22)"
 
+echo "LLVM_PREFIX=$LLVM_PREFIX" >> $GITHUB_ENV
 echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_TOOLCHAIN_FILE=$PWD/platforms/clang_mac.cmake" >> $GITHUB_ENV
 echo "MAKE_CMD=make -j$(sysctl -n hw.logicalcpu)" >> $GITHUB_ENV
+echo "CC=$LLVM_PREFIX/bin/clang" >> $GITHUB_ENV
+echo "CXX=$LLVM_PREFIX/bin/clang++" >> $GITHUB_ENV
