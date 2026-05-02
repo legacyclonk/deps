5f579f1ed6e126d60f6d20ee9ff2922a26e68832
Markus Mittendrein <git@maxmitti.at>
2026-05-02 16:45:27 +0200

    Update libjpeg-turbo to 3.1.4.1

--- scripts/libjpeg-turbo.sh
+++ scripts/libjpeg-turbo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=3.1.2
+VERSION=3.1.4.1
 until curl -L "https://github.com/libjpeg-turbo/libjpeg-turbo/releases/download/$VERSION/libjpeg-turbo-$VERSION.tar.gz" | tar -xz; do sleep 1; done
 
 mkdir build
