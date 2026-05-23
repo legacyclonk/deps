91cc88b3c23f261caafaf530c36373e6c904e325
Markus Mittendrein <git@maxmitti.at>
2026-05-24 01:23:22 +0200

    Mac x64: Correctly build for target OSX version 10.13

--- platforms/Mac-x64.sh
+++ platforms/Mac-x64.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 # nothing needed
-echo "TARGET_OSX_VERSION=10.9" >> $GITHUB_ENV
+echo "TARGET_OSX_VERSION=10.13" >> $GITHUB_ENV
+
+curl -L https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.13.sdk.tar.xz | tar xJ
+echo "OSX_SDK=$PWD/MacOSX10.13.sdk" >> $GITHUB_ENV
+
+mkdir clang-21
+pushd clang-21
+
+curl -L https://packages.macports.org/clang-21/clang-21-21.1.8_1+analyzer.darwin_17.x86_64.tbz2 | tar --strip-components 3 -x -j
+
+LLVM_CXX_INCLUDE=$PWD/libexec/llvm-21/include/c++/v1
+LLVM_CXX_LIB=$PWD/libexec/llvm-21/lib/libc++
+
+echo "LLVM_CXX_INCLUDE=$LLVM_CXX_INCLUDE" >> $GITHUB_ENV
+echo "LLVM_CXX_LIB=$LLVM_CXX_LIB" >> $GITHUB_ENV
+
+popd
+
+echo "LINK_FLAGS=-nostdlib++ $LLVM_CXX_LIB/libc++.a $LLVM_CXX_LIB/libc++abi.a $LLVM_CXX_LIB/libc++experimental.a --sysroot $PWD/MacOSX10.13.sdk" >> $GITHUB_ENV
+echo "C_XX_FLAGS=-nostdlib++ -nostdinc++ -I$LLVM_CXX_INCLUDE --sysroot $PWD/MacOSX10.13.sdk" >> $GITHUB_ENV
--- platforms/Mac.sh
+++ platforms/Mac.sh
@@ -4,8 +4,11 @@ export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 brew update
 brew install "llvm@$LLVM_VERSION" ninja
 LLVM_PREFIX="$(brew --prefix "llvm@$LLVM_VERSION")"
+NINJA_PREFIX="$(brew --prefix ninja)"
 
 echo "LLVM_PREFIX=$LLVM_PREFIX" >> $GITHUB_ENV
+echo "NINJA_PREFIX=$NINJA_PREFIX" >> $GITHUB_ENV
+
 echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_TOOLCHAIN_FILE=$PWD/platforms/clang_mac.cmake" >> $GITHUB_ENV
 echo "MAKE_CMD=make -j$(sysctl -n hw.logicalcpu)" >> $GITHUB_ENV
 echo "CC=$LLVM_PREFIX/bin/clang" >> $GITHUB_ENV
