c361231017d2f58c9cb9f4ade8dbfd295e5d134e
Markus Mittendrein <git@maxmitti.at>
2026-05-24 01:56:46 +0200

    openssl: Only build and install libraries and headers

--- scripts/openssl.sh
+++ scripts/openssl.sh
@@ -10,8 +10,7 @@ mkdir build
 pushd build
 
 CFLAGS="$C_XX_FLAGS" CXXFLAGS="$C_XX_FLAGS" LDFLAGS="$LINK_FLAGS" ../openssl-"$VERSION"/config --prefix="$OUTPUT_DIR" --libdir=lib no-dso no-module no-shared no-tests
-$MAKE_CMD
-$MAKE_CMD install_sw
+$MAKE_CMD install_dev
 
 popd
 
