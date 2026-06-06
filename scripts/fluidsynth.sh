d8d6c47655633de284deb3fe765de48b2ba4871f
Markus Mittendrein <git@maxmitti.at>
2026-06-06 17:19:44 +0200

    Update fluidsynth to latest version and enable SDL3 support

--- scripts/fluidsynth.sh
+++ scripts/fluidsynth.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=2.4.8
+VERSION=2.5.4
 
 dl_ext_source https://github.com/FluidSynth/fluidsynth/archive/v"$VERSION".tar.gz
 
@@ -14,7 +14,7 @@ mkdir build
 pushd build
 
 
-cmake ../fluidsynth-"$VERSION" -DLIB_SUFFIX="" -Denable-aufile=Off -Denable-libinstpatch=Off -Denable-libsndfile=Off -Denable-midishare=Off -Denable-sdl2=On -Denable-systemd=Off -Denable-readline=Off -Denable-alsa=Off -Denable-pulseaudio=Off -Denable-oss=Off -Denable-openmp=Off -Denable-ipv6=Off -Denable-network=Off -DCMAKE_SKIP_RPATH=On $CMAKE_CONFIGURE_ARGS
+cmake ../fluidsynth-"$VERSION" -DLIB_SUFFIX="" -Denable-aufile=Off -Denable-libinstpatch=Off -Denable-libsndfile=Off -Denable-midishare=Off -Denable-sdl3=On -Denable-systemd=Off -Denable-readline=Off -Denable-alsa=Off -Denable-pulseaudio=Off -Denable-pipewire=Off -Denable-oss=Off -Denable-openmp=Off -Denable-ipv6=Off -Denable-network=Off -Denable-jack=Off -Denable-dbus=Off -Denable-dsound=Off -Denable-wasapi=Off -Denable-waveout=Off -Denable-limiter=Off -Denable-framework=Off -DCMAKE_SKIP_RPATH=On $CMAKE_CONFIGURE_ARGS
 cmake --build . --target libfluidsynth $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
