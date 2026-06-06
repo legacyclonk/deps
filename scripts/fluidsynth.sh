f00fecc28b9b9f5a500d68ee5988a3bf9f98592a
Markus Mittendrein <git@maxmitti.at>
2026-06-06 21:15:52 +0200

    fluidsynth: Use C++ based OS abstraction

--- scripts/fluidsynth.sh
+++ scripts/fluidsynth.sh
@@ -14,7 +14,7 @@ mkdir build
 pushd build
 
 
-cmake ../fluidsynth-"$VERSION" -DLIB_SUFFIX="" -Denable-aufile=Off -Denable-libinstpatch=Off -Denable-libsndfile=Off -Denable-midishare=Off -Denable-sdl3=On -Denable-systemd=Off -Denable-readline=Off -Denable-alsa=Off -Denable-pulseaudio=Off -Denable-pipewire=Off -Denable-oss=Off -Denable-openmp=Off -Denable-ipv6=Off -Denable-network=Off -Denable-jack=Off -Denable-dbus=Off -Denable-dsound=Off -Denable-wasapi=Off -Denable-waveout=Off -Denable-limiter=Off -Denable-framework=Off -DCMAKE_SKIP_RPATH=On $CMAKE_CONFIGURE_ARGS
+cmake ../fluidsynth-"$VERSION" -DLIB_SUFFIX="" -Denable-aufile=Off -Denable-libinstpatch=Off -Denable-libsndfile=Off -Denable-midishare=Off -Denable-sdl3=On -Denable-systemd=Off -Denable-readline=Off -Denable-alsa=Off -Denable-pulseaudio=Off -Denable-pipewire=Off -Denable-oss=Off -Denable-openmp=Off -Denable-ipv6=Off -Denable-network=Off -Denable-jack=Off -Denable-dbus=Off -Denable-dsound=Off -Denable-wasapi=Off -Denable-waveout=Off -Denable-limiter=Off -Denable-framework=Off -Dosal=cpp11 -DCMAKE_SKIP_RPATH=On $CMAKE_CONFIGURE_ARGS
 cmake --build . --target libfluidsynth $CMAKE_BUILD_ARGS
 cmake --install . $CMAKE_BUILD_ARGS
 
