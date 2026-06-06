660903f8caae6721dae97034bb11d1ffc6117def
Markus Mittendrein <git@maxmitti.at>
2026-06-06 20:30:59 +0200

    Specify correct flags for meson

--- platforms/Linux.sh
+++ platforms/Linux.sh
@@ -7,7 +7,6 @@ curl -L "https://github.com/legacyclonk/gcc-build/releases/download/$GCC_VERSION
 
 echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_TOOLCHAIN_FILE=$PWD/platforms/custom_gcc.cmake" >> $GITHUB_ENV
 echo "MAKE_CMD=make -j$(nproc)" >> $GITHUB_ENV
-echo "MESON_CONFIGURE_ARGS=-Ddefault_library=static" >> $GITHUB_ENV
 
 GCC_MAJOR=$(echo "$GCC_VERSION" | cut -d . -f 1)
 echo "GCC_VERSION=$GCC_MAJOR" >> $GITHUB_ENV
